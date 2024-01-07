ca71267b2d8a8e34048f92de9bb78dfe0005accc
dipak-kumawat <dipaksk11@gmail.com>
2024-01-07 15:51:57 +0530

    adding api

---
+++ 1. weather app/script.js
@@ -0,0 +1,27 @@
+ const apiKey = "09888ebbfa50c9fe2b50480eaff81b2f";
+ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
+
+ async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) +"°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if(data.weather[0].main == "Clouds")
+
+ }
+
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
