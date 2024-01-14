ee989677058c94b954a4bda4a51ae69e4eed4e2c
dipak-kumawat <dipaksk11@gmail.com>
2024-01-14 13:02:01 +0530

    adding scripting

---
+++ 3. Tik Tak Toe/script.js
@@ -0,0 +1,23 @@
+// let boxes = document.querySelector(".box");
+// let resetBtn = document.querySelector("#reset-btn");
+
+// let turn = true;
+
+// const winPatterns = [
+//     [0, 1, 2],
+//     [0, 3, 6],
+//     [0, 4, 8],
+//     [1, 4, 8],
+//     [2, 5, 8],
+//     [2, 4, 6],
+//     [3, 4, 5],
+//     [6, 7, 8],
+// ];
+console.log ("hello")
+
+
+// boxes.forEach((box)  => {
+//     box.addEventListener("click", () =>{
+//         console.log("box");
+//     });
+// });
